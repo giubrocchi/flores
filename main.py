d21be6cb720c48044ac62c632023f2a3e2320480
Giulia Brocchi <61570069+giubrocchi@users.noreply.github.com>
2023-01-03 11:38:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
-from keras import Input, Model
+from keras import Sequential
 from keras.optimizers import Adam
-from keras.layers import Normalization, Conv2D, GlobalMaxPooling2D, BatchNormalization, Dense
+from keras.layers import Conv2D, GlobalMaxPooling2D, BatchNormalization, Dense, Dropout, RandomFlip, RandomRotation, RandomZoom
+from keras.applications.vgg16 import VGG16
+from keras.utils import to_categorical
 import cv2
 import os
 
@@ -22,7 +24,7 @@ images = []
 
 for path in os.listdir('./jpg'):
   image = cv2.imread('./jpg/' + path)[...,::-1]
-  image = cv2.resize(image, (500,500))
+  image = cv2.resize(image, (200, 200))/255
   images.append(image)
   labels.append(current_label)
 
@@ -32,29 +34,30 @@ for path in os.listdir('./jpg'):
 
 train, test, y_train, y_test = train_test_split(images, labels, test_size=0.2)
 
+y_train = to_categorical(y_train, 17)
+y_test = to_categorical(y_test, 17)
+
 x_train = tf.data.Dataset.from_tensor_slices((train, y_train))
-x_train = x_train.shuffle(len(x_train)).batch(16)
+x_train = x_train.batch(16)
+
 x_test = tf.data.Dataset.from_tensor_slices((test, y_test))
-x_test = x_test.shuffle(len(x_test)).batch(16)
+x_test = x_test.batch(16)
 
-input = Input(shape=(500, 500, 3))
-x = Normalization()(input)
-x = Conv2D(90, (10, 10), padding='same', activation='relu')(x)
-x = GlobalMaxPooling2D()(x)
-x = BatchNormalization()(x)
-outputs = Dense(17, activation="softmax", dtype='float32')(x)
+augmentation = Sequential([RandomFlip("horizontal"), RandomRotation(0.25), RandomZoom(0.2, 0.2)])
 
-model = Model(input, outputs)
-model.compile(Adam(learning_rate=0.001), loss='sparse_categorical_crossentropy', metrics='accuracy')
+trained = VGG16(input_shape = (200, 200, 3), include_top = False, weights = 'imagenet')
+trained.trainable = False
 
-class Callbacks(tf.keras.callbacks.Callback):
-  def on_epoch_end(self, epoch, logs=None):
-    if logs.get('accuracy') >= 0.85:
-      self.model.stop_training = True
+conv = Conv2D(32, (3, 3), padding='same', activation='relu')
+pool = GlobalMaxPooling2D()
+norm = BatchNormalization()
+drop = Dropout(0.3)
+outputs = Dense(17, activation="softmax", dtype='float32')
 
-callback = Callbacks()
+model = Sequential([augmentation, trained, conv, pool, norm, drop, outputs])
+model.compile(Adam(learning_rate=0.0005), loss='categorical_crossentropy', metrics='accuracy')
 
-history = model.fit(x_train, epochs=80, validation_data=x_test, verbose=1, callbacks=[callback])
+history = model.fit(x_train, epochs=30, validation_data=x_test, verbose=1)
 
 model.save('./model')
 
